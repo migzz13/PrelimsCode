18fd02bd9ab423d55ca5de0e79220df7c17b54b5
LUIS MIGUEL EMMANUEL MATIAS <miguelmatias3003@gmail.com>
2023-09-27 17:12:57 +0800

    Almost Finished

--- PrelimsCode/Program.cs
+++ PrelimsCode/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,64 +11,107 @@ namespace PrelimsCode
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Would you like to encrypt or decrypt a message? [E / D] : ");
-            
+            Console.Write("Would you like to encrypt or decrypt a message? [E / D] : ");
             string choice = Console.ReadLine().ToLower();
 
             switch (choice)
             {
                 case "e":
-                    Console.Write("Enter the encryption key (an integer): ");
-                    int encryptionKey = int.Parse(Console.ReadLine());
+                    Console.Write("Enter the encryption key (phrase or sentence): ");
+                    string encryptionKey = Console.ReadLine();
                     Console.Write("Enter the message to encrypt: ");
                     string messageToEncrypt = Console.ReadLine();
 
-                    string encryptedMessage = Encrypt(messageToEncrypt, encryptionKey);
-                    Console.WriteLine($"Encrypted message: {encryptedMessage}");
+                    EncryptMessage(encryptionKey, messageToEncrypt);
+                    Console.ReadKey();
                     break;
 
                 case "d":
-                    Console.Write("Enter the decryption key (an integer): ");
-                    int decryptionKey = int.Parse(Console.ReadLine());
-                    Console.Write("Enter the message to decrypt: ");
-                    string messageToDecrypt = Console.ReadLine();
+                    Console.Write("Enter the decryption key (phrase or sentence): ");
+                    string decryptionKey = Console.ReadLine();
 
-                    string decryptedMessage = Decrypt(messageToDecrypt, decryptionKey);
-                    Console.WriteLine($"Decrypted message: {decryptedMessage}");
+                    DecryptMessage(decryptionKey);
+                    Console.ReadKey();
                     break;
 
                 default:
-                    Console.WriteLine("Invalid Setting please try again. Press any key to continue.");
+                    Console.WriteLine("Invalid setting. Please try again. Press any key to continue.");
                     Console.ReadKey();
                     break;
             }
         }
 
-        static string Encrypt(string input, int key)
+        static void EncryptMessage(string key, string message)
+        {
+            string encryptedMessage = Encrypt(message, key);
+
+            // Save the encrypted message to a file
+            SaveToFile("encrypted_message.txt", encryptedMessage);
+
+            Console.WriteLine($"Encrypted message: {encryptedMessage}");
+        }
+
+        static void DecryptMessage(string key)
+        {
+            // Read the encrypted message from the file
+            string encryptedMessageFromFile = ReadFromFile("encrypted_message.txt");
+
+            string decryptedMessage = Decrypt(encryptedMessageFromFile, key);
+            Console.WriteLine($"Decrypted message: {decryptedMessage}");
+        }
+
+        static void SaveToFile(string fileName, string content)
+        {
+            using (StreamWriter writer = new StreamWriter(fileName))
+            {
+                writer.Write(content);
+            }
+        }
+
+        static string ReadFromFile(string fileName)
+        {
+            using (StreamReader reader = new StreamReader(fileName))
+            {
+                return reader.ReadToEnd();
+            }
+        }
+
+        static string Encrypt(string input, string key)
         {
             char[] result = input.ToCharArray();
+            int keyLength = key.Length;
 
             for (int i = 0; i < result.Length; i++)
             {
                 if (char.IsLetter(result[i]))
                 {
                     char offset = char.IsUpper(result[i]) ? 'A' : 'a';
-                    result[i] = (char)((result[i] + key - offset) % 26 + offset);
+                    int keyIndex = i % keyLength;
+                    int keyOffset = char.IsUpper(key[keyIndex]) ? 'A' : 'a';
+                    int shift = key[keyIndex] - keyOffset;
+
+                    result[i] = (char)((result[i] + shift - offset + 26) % 26 + offset);
                 }
             }
 
             return new string(result);
         }
-        static string Decrypt(string input, int key)
+
+        static string Decrypt(string input, string key)
         {
             char[] result = input.ToCharArray();
+            int keyLength = key.Length;
 
             for (int i = 0; i < result.Length; i++)
             {
                 if (char.IsLetter(result[i]))
                 {
                     char offset = char.IsUpper(result[i]) ? 'A' : 'a';
-                    int newPosition = (result[i] - offset - key + 26) % 26;
+                    int keyIndex = i % keyLength;
+                    int keyOffset = char.IsUpper(key[keyIndex]) ? 'A' : 'a';
+                    int shift = key[keyIndex] - keyOffset;
+
+                    int newPosition = (result[i] - offset - shift + 26) % 26;
                     result[i] = (char)(newPosition + offset);
                 }
             }
